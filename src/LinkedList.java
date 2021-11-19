1263891d2e735bc4b7908602bd7f12b096d4a141
viniciuslb <viniciuslb067@gmail.com>
2021-11-19 15:57:40 -0300

    add files

---
+++ src/LinkedList.java
@@ -0,0 +1,30 @@
+public class LinkedList {
+
+    private Element first;
+    private Element last;
+    private int length;
+
+    public Element getFirst() {
+        return first;
+    }
+
+    public void setFirst(Element first) {
+        this.first = first;
+    }
+
+    public Element getLast() {
+        return last;
+    }
+
+    public void setLast(Element last) {
+        this.last = last;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
